3529da3e83cae53b32f4a1f29037cbd14853c721
Olaf_T <70938349+Olaf-T@users.noreply.github.com>
2023-05-16 08:21:25 +0200

    added nothing

---
+++ UFO/Assets/Scripts/audiomanager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] music;
+    public Sound[] ufo_move;
+    public Sound[] coin_pickup;
+    public Sound[] wall_hit;
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
